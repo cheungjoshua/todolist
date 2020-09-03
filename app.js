6ddb164061e6522863b500497ed07bff6de98d31
Joshua C <contact.cjoshua@gmail.com>
2020-09-03 13:27:59 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//Selectors
+const todoInput = document.querySelector('todo-input'); 
+const todoButton = document.querySelector('todo-button'); 
+const todoList = document.querySelector('todo-list'); 
+
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+
+function addTodo(e) {
+    //prevent refresh
+    e.preventDefault();
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //creat Li
+}
