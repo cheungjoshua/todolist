b24d0c0fa22cad5cfb6ce46e846de010d70fcd22
Joshua C <contact.cjoshua@gmail.com>
2020-09-07 16:59:30 -0700

    finish the function

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 //Selectors
-const todoInput = document.querySelector('todo-input'); 
-const todoButton = document.querySelector('todo-button'); 
-const todoList = document.querySelector('todo-list'); 
+const todoInput = document.querySelector('.todo-input'); 
+const todoButton = document.querySelector('.todo-button'); 
+const todoList = document.querySelector('.todo-list'); 
 
 
 //Event Listeners
-todoButton.addEventListener('click', addTodo);
+todoButton.addEventListener("click", addTodo);
 
 //Functions
 
-function addTodo(e) {
+function addTodo(evt) {
     //prevent refresh
-    e.preventDefault();
+    evt.preventDefault();
     //Todo div
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
     //creat Li
     const newTodo = document.createElement('li');
-    newTodo.innerText = 'hey';
+    newTodo.innerText = todoInput.value;
+    //Add Li to Todo
+    todoDiv.appendChild(newTodo);
+    //Add Todo div to todoList
+    todoList.appendChild(todoDiv);
 }
